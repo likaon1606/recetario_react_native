c0ecbef0ae80b36e6c45539108fa668df615f139
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-07-08 17:47:05 -0600

    contact

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,29 @@
-// import { useRouter } from 'expo-router';
-import { Image, ImageBackground, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <ImageBackground
       source={require('../../assets/images/cocina_fondo_principal.jpg')}
       style={styles.background}
-      resizeMode="cover"
+      resizeMode='cover'
     >
       <View style={styles.overlay}>
         <Text style={styles.title}>Mi Recetario Personal</Text>
-        <Text style={styles.subtitle}>Organiza tus recetas y planifica tus comidas</Text>
+        <Text style={styles.subtitle}>
+          Organiza tus recetas y planifica tus comidas
+        </Text>
 
         <View style={styles.contactContainer}>
-          <Text style={styles.contactTitle}>¿Necesitas un sitio web o app a medida?</Text>
-          <Text style={styles.contactText}>Ofrezco desarrollo profesional y personalizado.</Text>
-
-          <TouchableOpacity onPress={() => Linking.openURL('https://wa.me/525532604568')} style={styles.whatsappContainer}>
-            <Image
-              source={require('../../assets/images/whats.png')}
-              style={styles.whatsappIcon}
-            />
-            <Text style={styles.whatsappText}>Contáctame: 5532604568</Text>
-          </TouchableOpacity>
+          <Text style={styles.contactTitle}>
+            ¿Necesitas un sitio web o app a medida?
+          </Text>
+          <Text style={styles.contactText}>
+            ventas@hechoenlinea.com
+          </Text>
+          <Text style={styles.contactText}>🌐 hechoenlinea.com</Text>
         </View>
       </View>
     </ImageBackground>
@@ -47,6 +48,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#A84E0E',
     marginBottom: 8,
+    fontFamily: 'sans-serif',
     textAlign: 'center',
   },
   subtitle: {
@@ -54,6 +56,7 @@ const styles = StyleSheet.create({
     color: '#6F4E37',
     marginBottom: 24,
     textAlign: 'center',
+    fontFamily: 'sans-serif-light',
   },
   contactContainer: {
     width: '100%',
@@ -70,9 +73,9 @@ const styles = StyleSheet.create({
   },
   contactText: {
     fontSize: 15,
-    color: 'blue',
+    color: '#333',
     textAlign: 'center',
-    marginBottom: 6,
+    marginBottom: 10,
   },
   whatsappContainer: {
     flexDirection: 'row',
@@ -82,9 +85,8 @@ const styles = StyleSheet.create({
     marginTop: 6,
   },
   whatsappIcon: {
-    width: 20,
-    height: 20,
-    marginRight: 6,
+    fontSize: 18,
+    color: '#25D366', // verde WhatsApp
   },
   whatsappText: {
     fontSize: 15,
